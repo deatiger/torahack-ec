b01dc32fc9a7e9d6de971f5433fe7c641038b34b
g9m8kqhskjrf <g9m8kqhskjrf@yahoo.co.jp>
2020-07-07 21:12:03 +0900

    Set redux-thunk

--- src/templates/Login.jsx
+++ src/templates/Login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useDispatch} from "react-redux";
-import {push} from "connected-react-router";
-import {signInAction} from "../reducks/users/actions";
+import { signIn } from '../reducks/users/operation';
+
 
 const Login = () => {
      const dispatch = useDispatch();
@@ -9,10 +9,7 @@ const Login = () => {
      return (
          <div>
              <h2>ログイン</h2>
-             <button onClick={() => {
-                dispatch(signInAction({uid: "0001", username: "torahack"}));
-                dispatch(push('/'))  
-             }}>
+             <button onClick={() => dispatch(signIn())}>
                  ログインする
              </button>
          </div>
