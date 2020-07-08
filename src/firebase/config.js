2a476d9cdb01f9a43ac8b2bb7e7c37d680d80e08
g9m8kqhskjrf <g9m8kqhskjrf@yahoo.co.jp>
2020-07-08 16:59:56 +0900

    Setting firebase auth

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAoeCUMi-LRcwrnaV7cyiY4uUvjGuCDPoQ",
+    authDomain: "ec-app-0707.firebaseapp.com",
+    databaseURL: "https://ec-app-0707.firebaseio.com",
+    projectId: "ec-app-0707",
+    storageBucket: "ec-app-0707.appspot.com",
+    messagingSenderId: "680690732571",
+    appId: "1:680690732571:web:7dc3e43f7b5ef4822f32be",
+    measurementId: "G-Z1KJE78XX2"
+  };
