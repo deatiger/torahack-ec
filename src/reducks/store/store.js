968858d587197b34ebdf78556e74c8fcc6be4e07
g9m8kqhskjrf <g9m8kqhskjrf@yahoo.co.jp>
2020-07-10 15:41:35 +0900

    Add and rewrite products

--- src/reducks/store/store.js
+++ src/reducks/store/store.js
@@ -6,12 +6,14 @@ import {
 import {connectRouter, routerMiddleware} from 'connected-react-router'
 import thunk from 'redux-thunk';
 
+import {ProductsReducer} from "../products/reducers";
 import {UsersReducer} from "../users/reducers";
 
 // 現在どこのページにいるのか？というような情報を持っているのがhistory
 export default function createStore(history) {
     return reduxCreateStore(
         combineReducers({
+            products: ProductsReducer,
             router: connectRouter(history),
             // stateのプロパティ(users)がここでkeyに入ってくる
             users: UsersReducer
